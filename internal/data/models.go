c0d177a096865777d33108f11f86967929cdb854
Bhat Owais <owaisbhat996@outlook.com>
2024-04-28 10:42:30 +0530

    Added permissions table

--- internal/data/models.go
+++ internal/data/models.go
@@ -21,16 +21,18 @@ type Models struct {
 		Update(movie *Movie) error
 		Delete(id int64) error
 	}
-	Users  UserModel
-	Tokens TokenModel
+	Users       UserModel
+	Tokens      TokenModel
+	Permissions PermissionModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Users:  UserModel{DB: db},
-		Tokens: TokenModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Users:       UserModel{DB: db},
+		Tokens:      TokenModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 	}
 }
 
